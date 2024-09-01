3a4c1d39b3fbbe2284bc5da6a3ab2c0be660f29a
Diego Lalanda <guaio_2014@hotmail.com>
2024-09-01 18:23:43 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const sequelize = require('./config/db');
+
+app.use(express.json());
+
+// Sincronizar la base de datos
+sequelize.sync()
+  .then(() => console.log('Base de datos sincronizada.'))
+  .catch(err => console.error('Error al sincronizar la base de datos:', err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
