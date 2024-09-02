45972844024adda7663c93c38d08ff9dd1481d6e
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2024-09-02 12:39:48 -0300

    Update usuariosTipo.routes.js

--- routes/usuariosTipo.routes.js
+++ routes/usuariosTipo.routes.js
@@ -2,25 +2,21 @@ const express = require('express');
 const router = express.Router();
 const {
   getAllUsuariosTipo,
-  getUsuarioTipoById, // Asegúrate de que esta función está exportada en usuariosController.js
+  getUsuarioTipoById, 
   createUsuarioTipo,
   updateUsuarioTipo,
   deleteUsuarioTipo,
-} = require('../controllers/usuariosController'); // Cambia el nombre del archivo si es necesario
+} = require('../controllers/usuariosController'); 
 
-// Ruta para obtener todos los tipos de usuario
+
 router.get('/', getAllUsuariosTipo);
 
-// Ruta para obtener un tipo de usuario por ID
 router.get('/:id', getUsuarioTipoById);
 
-// Ruta para crear un nuevo tipo de usuario
 router.post('/', createUsuarioTipo);
 
-// Ruta para actualizar un tipo de usuario
 router.put('/:id', updateUsuarioTipo);
 
-// Ruta para eliminar un tipo de usuario
 router.delete('/:id', deleteUsuarioTipo);
 
 module.exports = router;
