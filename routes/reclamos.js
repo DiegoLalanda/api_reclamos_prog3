50815a73d3d05954577dec412dfd6b9cdf954e73
FernandaElola <elolafernanda@gmail.com>
2024-11-02 20:27:51 -0300

    avances reclamos

--- routes/reclamos.js
+++ routes/reclamos.js
@@ -1,23 +1,22 @@
 import express from 'express';
 import ReclamosController from '../controllers/reclamosController.js';
-import { isAdmin } from '../middlewares/authMiddleware.js'; // Asegúrate de que tienes el middleware isAdmin
+import { isAdmin, isClient, isEmployee } from '../middlewares/authMiddleware.js';
 import protectedRoutes from '../utils/protectedRoutes.js';
 
 const router = express.Router();
 const reclamosController = new ReclamosController();
 
 // Rutas de reclamos
-protectedRoutes.get('/reclamos', reclamosController.findAllReclamos);
-protectedRoutes.get('/reclamos/:idReclamo', reclamosController.findByIdReclamo);
-protectedRoutes.post('/reclamos', reclamosController.createReclamo);
-protectedRoutes.put('/reclamos/:idReclamo', reclamosController.updateReclamo);
-protectedRoutes.put('/reclamos/:idReclamo/estado', reclamosController.actualizarEstadoReclamo);
+protectedRoutes.get('/reclamos', isAdmin, reclamosController.findAllReclamos); 
+protectedRoutes.get('/reclamos/:idReclamo', isAdmin, reclamosController.findByIdReclamo); 
+protectedRoutes.post('/reclamos', isClient, reclamosController.createReclamo); 
+protectedRoutes.put('/reclamos/:idReclamo', reclamosController.updateReclamo); 
+protectedRoutes.put('/reclamos/:idReclamo/estado', isClient, reclamosController.actualizarEstadoReclamo); 
+protectedRoutes.get('/reclamos/:idReclamo/estado', isClient, reclamosController.consultarEstadoReclamo); 
+protectedRoutes.get('/reclamos/oficina/:idOficina', isEmployee, reclamosController.atenderReclamos); 
 
 // Ruta para descargar el informe PDF
-protectedRoutes.get('/informe-pdf', 
-    isAdmin, 
-    reclamosController.descargarInformeReclamos
-);
+protectedRoutes.get('/informe-pdf', isAdmin, reclamosController.descargarInformeReclamos); 
 
 router.use('/secure', protectedRoutes);
 
