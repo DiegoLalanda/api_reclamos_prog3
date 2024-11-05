bf76e5ea93c8a183b4f779dffd14b82c331031df
FernandaElola <elolafernanda@gmail.com>
2024-11-05 19:20:50 -0300

    asignacion y update de relacion usuario oficina

--- routes/estadisticas.js
+++ routes/estadisticas.js
@@ -1,38 +1,37 @@
 // estadisticasRoutes.js
 import express from 'express';
-import passport from 'passport';
 import {
     getEstadisticasReclamosPorTipo,
     getEstadisticasReclamosPorEstado,
     getEstadisticasTiempoPromedioReclamos
 } from '../controllers/estadisticasController.js';
 import { isAdmin } from '../middlewares/authMiddleware.js';
+import protectedRoutes from '../utils/protectedRoutes.js';
 import errorMiddleware from '../middlewares/errorMiddleware.js';
 
 const router = express.Router();
 
 // Ruta para obtener el total de reclamos por tipo
-router.get('/estadisticas/reclamosTipo', 
-    passport.authenticate('jwt', { session: false }), 
+protectedRoutes.get('/estadisticas/reclamosTipo', 
     isAdmin, 
     getEstadisticasReclamosPorTipo,
     errorMiddleware
 );
 
 // Ruta para obtener el total de reclamos por estado
-router.get('/estadisticas/reclamosEstado', 
-    passport.authenticate('jwt', { session: false }), 
+protectedRoutes.get('/estadisticas/reclamosEstado', 
     isAdmin, 
     getEstadisticasReclamosPorEstado,
     errorMiddleware
 );
 
 // Ruta para obtener el tiempo promedio de resolución de reclamos
-router.get('/estadisticas/tiempoPromedio', 
-    passport.authenticate('jwt', { session: false }), 
+protectedRoutes.get('/estadisticas/tiempoPromedio', 
     isAdmin, 
     getEstadisticasTiempoPromedioReclamos,
     errorMiddleware
 );
+
+router.use('/secure', protectedRoutes);
 
 export default router;
