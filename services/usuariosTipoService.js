463156ea4cab10a5860c9856b62023fc03604a88
Diego Lalanda <guaio_2014@hotmail.com>
2024-11-05 11:13:39 -0300

    separo service y data (ultimas 2 tablas)

--- services/usuariosTipoService.js
+++ services/usuariosTipoService.js
@@ -1,49 +1,32 @@
-import connectToDatabase from '../config/db.js';
+// services/usuariosTipoService.js
+import UsuariosTipoData from '../database/usuariosTipoData.js';
 
 export default class UsuariosTipoService {
     // Obtener todos los tipos de usuario
     async findAll() {
-        const connection = await connectToDatabase();
-        const [results] = await connection.execute('SELECT * FROM usuarios_tipo');
-        return results;
+        return await UsuariosTipoData.findAll();
     }
 
     // Obtener un tipo de usuario por ID
     async findById(id) {
-        const connection = await connectToDatabase();
-        // Cambiado de idReclamoTipo a idUsuarioTipo
-        const [results] = await connection.execute('SELECT * FROM usuarios_tipo WHERE idUsuarioTipo = ?', [id]);
-        return results[0]; // Retornar el primer resultado si se encuentra
+        return await UsuariosTipoData.findById(id);
     }
 
     // Crear un nuevo tipo de usuario
     async create(usuarioTipo) {
         const { descripcion, activo } = usuarioTipo;
-        const connection = await connectToDatabase();
-        // Cambiado de idReclamoTipo a idUsuarioTipo en el retorno del ID creado
-        const [result] = await connection.execute(
-            'INSERT INTO usuarios_tipo (descripcion, activo) VALUES (?, ?)',
-            [descripcion, activo]
-        );
-        return { idUsuarioTipo: result.insertId, descripcion, activo };
+        return await UsuariosTipoData.create(descripcion, activo);
     }
 
     // Actualizar un tipo de usuario
     async update(id, usuarioTipo) {
         const { descripcion, activo } = usuarioTipo;
-        const connection = await connectToDatabase();
-        // Cambiado de idReclamoTipo a idUsuarioTipo
-        await connection.execute(
-            'UPDATE usuarios_tipo SET descripcion = ?, activo = ? WHERE idUsuarioTipo = ?',
-            [descripcion, activo, id]
-        );
-        return this.findById(id); // Retornar el tipo de usuario actualizado
+        await UsuariosTipoData.update(id, descripcion, activo);
+        return await this.findById(id); // Retorna el tipo de usuario actualizado
     }
 
     // Eliminar un tipo de usuario
     async destroy(id) {
-        const connection = await connectToDatabase();
-        // Cambiado de idReclamoTipo a idUsuarioTipo
-        await connection.execute('DELETE FROM usuarios_tipo WHERE idUsuarioTipo = ?', [id]);
+        await UsuariosTipoData.destroy(id);
     }
 }
